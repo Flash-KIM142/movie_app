4b94115d249f757a962db4c9d416375715cadde0
Flash KIM <62537294+Flash-KIM142@users.noreply.github.com>
2020-08-04 18:32:50 +0900

    Updates

---
+++ precache-manifest.4288dc71b880cb1ee2d8f0c1715d2016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13caf05204f0c1ab89cfc699cc37f1ac",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8e9d0284a0a736f76e25",
+    "url": "/movie_app/static/css/main.c45a9ac3.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9d0284a0a736f76e25",
+    "url": "/movie_app/static/js/main.1559da05.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
